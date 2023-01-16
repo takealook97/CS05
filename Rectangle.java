9c05a1975b141368412c957bab225cae1ff116ff
takealook <takealook97@naver.com>
2023-01-16 13:13:10 +0900

    Rectangle class

--- Rectangle.java
+++ Rectangle.java
@@ -1,2 +1,8 @@
 public class Rectangle {
+    double triangle;
+
+    double getSize(String[] input) {
+        triangle = new Triangle().getSize(new String[]{input[0], input[1], input[2]});
+        return 2 * triangle;
+    }
 }
